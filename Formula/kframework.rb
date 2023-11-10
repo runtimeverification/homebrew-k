ab4337d34481bf4f822ba68e231015fdc97f1035
rv-jenkins <devops@runtimeverification.com>
2023-11-10 13:15:11 +0000

    Update brew package version

--- Formula/kframework.rb
+++ Formula/kframework.rb
@@ -1,12 +1,12 @@
 class Kframework < Formula
   desc "K Framework Tools 5.0"
   homepage ""
-  url "https://github.com/runtimeverification/k/releases/download/v6.1.4/kframework-6.1.4-src.tar.gz"
-  sha256 "ed140edea60b99241fe78fc9930237b0c50aa4105d8ec560a951032934015684"
+  url "https://github.com/runtimeverification/k/releases/download/v6.1.5/kframework-6.1.5-src.tar.gz"
+  sha256 "ccd4eefd02f47d095809e28e82a5b5b852c78000121f221f69b948e4eb9dc10e"
   bottle do
-    root_url "https://github.com/runtimeverification/k/releases/download/v6.1.4/"
-    rebuild 646
-    sha256 ventura: "ec5cc7a8be5f860957603ea2dc12b3561e6f6e1f6adecf2789b69586f569280f"
+    root_url "https://github.com/runtimeverification/k/releases/download/v6.1.5/"
+    rebuild 647
+    sha256 ventura: "3dfd9cb3b1e64e7a8a3357bb5ef0983f5d8a7425c8479d2ee4a6c70be0f0b0e9"
   end
   depends_on "cmake" => :build
   depends_on "haskell-stack" => :build
